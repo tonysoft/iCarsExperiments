757b2bf3534e38bb4cb70215e07113fc5819b128
tonysoft <tony@iRelate.us>
2014-09-29 14:27:15 -0700

    Annotated and Commented...
    Explain how the Parameterization was done and the TODO list to fully
    generalize it…

--- server/explorer.js
+++ server/explorer.js
@@ -20,7 +20,10 @@ module.exports = function mountLoopBackExplorer(server, next) {
     // If we found some SOAP Connectors...
 
     for (var i = 0; i < numSoapConnectors; i++) {
+
       waitForSOAPConnectorToLoad(i);
+        // Handle each one in its own scope...
+
       function waitForSOAPConnectorToLoad(i) {
         var soapConnector = soapConnectors[i];
         soapConnector.once('connected', function () {
@@ -28,68 +31,86 @@ module.exports = function mountLoopBackExplorer(server, next) {
           var connectorModel = soapConnector.createModel(soapConnectorNames[i], {});
           var services = Object.keys(soapConnector.adapter.client.wsdl.services);
           for (var j = 0; j < services.length; j++) {
-            var serviceName = services[j];
-            var bindingName = serviceName + "Soap";
-            var soapMethods = soapConnector.adapter.client.wsdl.definitions.bindings[bindingName].methods;
-            for (var soapMethod in soapMethods) {
-              creatParameterizedSoapRemoteMethod(soapMethod);
-              function creatParameterizedSoapRemoteMethod(soapMethod) {
-                var method = connectorModel.sharedClass.find(soapMethod, true);
-                if (method !== null) {
-                  method = method.fn;
-                  method.shared = false;
-                  var params = soapConnector.adapter.client.wsdl.definitions.bindings[bindingName].methods[soapMethod].input.$lookupTypes;
-                  if (params.length > 0) {
-                    var accepts = [];
-                    for (var p = 0; p < params.length; p++) {
-                      var param = {arg: '', type: '', required: true, http: {source: 'query'}};
-                      param.arg = params[p].$name;
-                      param.type = params[p].$type.replace('s:', '');
-                      accepts.push(param);
+            // Handle each service...
+
+            parameterizeSoapService(j);
+              // in its own scope...
+
+            function parameterizeSoapService(j) {
+              var serviceName = services[j];
+                // This is the Service Name somethin like "Weather"...
+              var bindingName = serviceName + "Soap";
+                // Let's only handle the "Soap" binding now and leave the "Soap12" binding alone.
+
+              var soapMethods = soapConnector.adapter.client.wsdl.definitions.bindings[bindingName].methods;
+              for (var soapMethod in soapMethods) {
+                // Parameterize each method...
+
+                creatParameterizedSoapRemoteMethod(soapMethod);
+                  // in its own scope...
+
+                function creatParameterizedSoapRemoteMethod(soapMethod) {
+                  var method = connectorModel.sharedClass.find(soapMethod, true);
+                    // find the corresponding method in the Loopback Model we attached to the Connector...
+
+                  if (method !== null) {
+                    method = method.fn;
+
+                    var params = soapConnector.adapter.client.wsdl.definitions.bindings[bindingName].methods[soapMethod].input.$lookupTypes;
+                    if (params.length > 0) {
+                      // if we found input parameters...
+
+                      method.shared = false;
+                        // Hide the standard Method generated in the Model...
+
+                      var accepts = [];
+                      for (var p = 0; p < params.length; p++) {
+                        var param = {arg: '', type: '', required: true, http: {source: 'query'}};
+                        param.arg = params[p].$name;
+                        param.type = params[p].$type.replace('s:', '');
+                        accepts.push(param);
+                      }
+                        // generate a Loopback Style Input Parameter Array
+                        // TODO Below...
+                        // *********** We need to test with a variety of Soap Services
+                        // *********** to ensure the the $type maps correctly from the
+                        // *********** Soap standards to our Javascript types.
+                        // Right now we know "string" type parameters work, but others might fail.
+
+                      // The Signature for the new Remote Methods will PREPEND an UNDERSCORE
+                      connectorModel['_' + soapMethod] = function () {
+                        var tempArguments = arguments;
+                          // Cache the input arguments...
+
+                        var inputObject = {}
+                        for (var ip = 0; ip < accepts.length; ip++) {
+                          inputObject[accepts[ip].arg] = tempArguments[ip];
+                        }
+                          // Generate the Object to pass to the Standard, Unparameterized Method.
+
+                        connectorModel[soapMethod](inputObject, function (err, response) {
+                          // Call the Standard Method...
+
+                          tempArguments[tempArguments.length-1](err, response);
+                            // The Callback will always be the last input Argument.
+                        });
+                      };
+
+                      loopback.remoteMethod(
+                        connectorModel['_' + soapMethod], {
+                          accepts: accepts,
+                            // This is the Loopback Style Parameter List...
+                          returns: {arg: 'result', type: 'object', root: true},
+                            // TODO: Test for other Soap Return Types...
+                          http: {verb: 'get', path: '/' + '_' + soapMethod}
+                        }
+                      );
                     }
-
-                    connectorModel['_' + soapMethod] = function () {
-                      var tempArguments = arguments;
-
-                      var inputObject = {}
-                      for (var ip = 0; ip < accepts.length; ip++) {
-                        inputObject[accepts[ip].arg] = tempArguments[ip];
-                      }
-                      console.log(soapMethod + ":" + inputObject);
-                      connectorModel[soapMethod](inputObject, function (err, response) {
-                        console.log('Weather: %j', response);
-                        var result = response;
-                        tempArguments[tempArguments.length-1](err, result);
-                      });
-                    };
-
-                    loopback.remoteMethod(
-                      connectorModel['_' + soapMethod], {
-                        accepts: accepts,
-                        returns: {arg: 'result', type: 'object', root: true},
-                        http: {verb: 'get', path: '/' + '_' + soapMethod}
-                      }
-                    );
                   }
                 }
               }
             }
           }
-//           var method = connectorModel.sharedClass.find('GetCityWeatherByZIP', true);
-//           if (method !== null) {
-//             method = method.fn;
-// //            method.shared = false;
-//             console.log(connectorModel['GetCityWeatherByZIP']);
-//             method.accepts = [
-//                 {arg: 'ZIP', type: 'string', required: true,
-//                   http: {source: 'form'}}
-//               ]
-// //            method.fn.http = { "path": soapConnector.adapter.client.wsdl.options.wsdl.replace('?WSDL', '/') +  'GetCityWeatherByZIP' };
-//             console.log(method);
-//           }
-//           console.log(soapConnector.adapter.client.wsdl.definitions.bindings);
-//           console.log(soapConnector.adapter.client.wsdl.definitions.bindings.WeatherSoap.methods['GetCityWeatherByZIP'].input); //messages.GetCityWeatherByZIP.element.children[0].children[0]);
-//           console.log(Object.keys(soapConnector.adapter.client.wsdl.services)[0]); //.definitions.bindings.WeatherSoap.methods.GetCityWeatherByZIP.input.$lookupTypes); //messages.GetCityWeatherByZIP.element.children[0].children[0]);
           server.model(connectorModel);
           // Let's associate a Model and expose it so it'll appear in Explorer
 
